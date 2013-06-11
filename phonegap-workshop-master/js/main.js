df014fae18bb2ce24e35ab725a726886536cbb51
Jack Walters <jack@lastweb.com>
2013-06-11 10:15:10 -0700

    register scroll events

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -9,8 +9,28 @@ var app = {
         }
     },
     
+    registerEvents: function() {
+        var self = this;
+        //check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listnerer to change the "selected state of the item
+            $('body').on('touchend', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register a mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+    
     initialize: function() {
         var self = this;
+        self.registerEvents();
         this.store = new MemoryStore(function() {
  //           self.showAlert('Store Initialized', 'Info');
             $('body').html(new HomeView(self.store).render().el);
