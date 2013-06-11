b9e79d9cd3fdf9e79e8a9d4fd98ebd7bcd60fe66
Jack Walters <jack@lastweb.com>
2013-06-10 19:04:32 -0700

    new homeview.js

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,9 +1,4 @@
-var app = {
-    
-    renderHomeView: function () {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+var app = {    
 
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -13,24 +8,14 @@ var app = {
             alert(title + ": " + message);
         }
     },
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
+    
     initialize: function() {
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         var self = this;
         this.store = new MemoryStore(function() {
  //           self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-       
     }
-
 };
 
 app.initialize();
